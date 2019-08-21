0add4d054cdc90ab8625852caafc17408af3ca31
Angus Yiu <angus_yl_yiu@wfsfaa.gov.hk>
2019-08-21 17:30:10 +0800

    basic google map embedded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {} from 'googlemaps';  
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'riders-world';
+  @ViewChild('map') mapElement: any;
+  map: google.maps.Map;  
+  checkPoints = [
+    {lat: 22.3193, lng: 114.1694},    
+    {lat: 21.3193, lng: 114.1694}
+  ];
+
+  ngOnInit(): void {
+    const mapProperties = {
+         center: new google.maps.LatLng(22.3193, 114.1694),
+         zoom: 15,
+         mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
+    this.checkPoints.map(chkPt=>{
+      var checkPoint = new google.maps.Marker(
+        {
+          position: {lat: chkPt.lat, lng: chkPt.lng},
+          map: this.map
+        });
+    });
+ }
+ 
 }
