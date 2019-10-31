72399dbf52bdae44b0c6ad25147052637a1e6184
Angus Yiu <angus_yl_yiu@wfsfaa.gov.hk>
2019-10-31 19:09:35 +0800

    add map control - place search box

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PostListComponent } from './shared/components/post-list/post-list.component';
-import { SearchComponent } from './shared/components/search/search.component';
+import { MapComponent } from './shared/components/map/map.component';
 import { SettingComponent } from './shared/components/setting/setting.component';
 import { SignupComponent } from './shared/components/signup/signup.component';
 import { SigninComponent } from './shared/components/signin/signin.component';
@@ -9,8 +9,8 @@ import { UserInfoComponent } from './shared/components/user-info/user-info.compo
 import { AuthGuard } from './shared/helpers';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/search', pathMatch: 'full' },
-  { path: 'search', component: SearchComponent },
+  //{ path: '', redirectTo: '/search', pathMatch: 'full' },
+  { path: 'search', component: MapComponent },
   { path: 'post-list', component: PostListComponent },
   { path: 'setting', component: SettingComponent },
   { path: 'signup', component: SignupComponent },
